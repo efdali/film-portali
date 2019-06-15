4fffc23f4ec56e8d680a6386c4d9eb316ba6b47a
efdali <efdal_incesu@hotmail.com>
2019-06-15 12:58:13 +0300

    kategoriler

--- js/main.js
+++ js/main.js
@@ -4,4 +4,35 @@ window.addEventListener('DOMContentLoaded', (event) => {
 
 $("#kinds").click(function(){
     $(".kind-container").toggle();
-})                  
+});          
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:15,
+    dots:true,
+    autoplay:true,
+    autoplayTimeout:3000,
+    autoplayHoverPause:true,
+    responsiveClass:true,
+    responsive:{
+        0:{
+            items:1,
+            nav:true
+        },
+        600:{
+            items:3,
+            nav:true
+        },
+        1000:{
+            items:6,
+            nav:true,
+            loop:true
+        }
+    }
+});
+
+$("#search").focus(function(){
+    $(".search-film-list").css("display","block");
+});
+$("#search").blur(function(){
+    $(".search-film-list").css("display","none");
+});
